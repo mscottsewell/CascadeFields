c75eaa3aad4a702e5ec48f207be07a7064214a84
Scott Sewell <mscottsewell@users.noreply.github.com>
2025-12-31 14:59:22 -0600

    Add new icons and update existing ones for CascadeFields
    - Added new PNG icons: CascadeFields_AddRelationship_24.png, CascadeFields_LoadConfig_24.png, CascadeFields_LoadJSON_24.png, CascadeFields_PublishConfig_24.png, CascadeFields_RemoveRelationship_24.png, and CascadeFields_SaveJSON_24.png.
    - Introduced a new SVG icon: CascadeFields_Arrows.svg.
    - All icons were created or modified using Inkscape for consistency in design.

--- CascadeFields.Plugin/Properties/AssemblyInfo.cs
+++ CascadeFields.Plugin/Properties/AssemblyInfo.cs
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.2026.1.20")]
+[assembly: AssemblyFileVersion("1.2026.1.21")]
+
 
 
 
